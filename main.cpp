f037a234282a19b9e44b7990e685765f98731986
Henryawd345 <henrygoodboy2000@gmail.com>
2025-09-26 02:00:22 -0700

    Fixed the output method

--- main.cpp
+++ main.cpp
@@ -21,13 +21,14 @@ public:
     void setBluev(int b) {bluev = b;}
 
     void print() {
-        cout << setw(15) << "Red Value: " << redv << endl;
-        cout << setw(15) << "Green Value: " << greenv << endl;
+        cout << setw(15) << "Color >> " << name;
+        cout << setw(15) << "Red Value: " << redv;
+        cout << setw(15) << "Green Value: " << greenv;
         cout << setw(15) << "Blue Value: " << bluev << endl;
     }
 };
 
-void print(Color);
+//void print(Color);
 
 int main() {
     cout << fixed << setprecision(2);
@@ -36,25 +37,25 @@ int main() {
     RedCl.setRedv(255);
     RedCl.setGreenv(0);
     RedCl.setBluev(0);
-    print(RedCl);
+    RedCl.print();
 
     Color GreenCl;
     GreenCl.setcolorName("Green");
     GreenCl.setRedv(0);
     GreenCl.setGreenv(255);
     GreenCl.setBluev(0);
-    print(GreenCl);
+    GreenCl.print();
 
     Color BlueCl;
     BlueCl.setcolorName("Blue");
     BlueCl.setRedv(0);
     BlueCl.setGreenv(0);
     BlueCl.setBluev(255);
-    print(BlueCl);
+    BlueCl.print();
 
     return 0;
 }
 
-void print(Color c){
-    cout << c.getcolorName()<< " RGB value: " << c.getRedv() << " " << c.getGreenv() << " " << c.getBluev() << " " << endl;
-}
+//void print(Color c){
+    //cout << c.getcolorName()<< " RGB value: " << c.getRedv() << " " << c.getGreenv() << " " << c.getBluev() << " " << endl;
+//}
