98c5e26d68a9c8458dcb2f47ea0b5952eca04486
Henryawd345 <henrygoodboy2000@gmail.com>
2025-09-26 01:10:56 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iomanip>
 
+using namespace std;
+
 
 class Color{
 private:
@@ -11,4 +13,14 @@ public:
     int getRedv() {return redv;}
     int getGreenv() {return greenv;}
     int getBluev() {return bluev;}
+    void setRedv(int r) {redv = r;}
+    void setGreenv(int g) {greenv = g;}
+    void setBluev(int b) {bluev = b;}
+
+    void print() {
+        cout << setw(15) << "Red Value: " << redv << endl;
+        cout << setw(15) << "Green Value: " << greenv << endl;
+        cout << setw(15) << "Blue Value: " << bluev << endl;
+    }
+
 };
